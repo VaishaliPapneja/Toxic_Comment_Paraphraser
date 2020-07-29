57de8185a4cbb9dcdf07588bbd9061a171872ead
Vaishali Papneja <51495198+VaishaliPapneja@users.noreply.github.com>
2020-07-29 16:16:31 +0530

    Add files via upload

---
+++ bestSyn.py
@@ -0,0 +1,87 @@
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns',None)
+
+from nltk.stem.snowball import SnowballStemmer
+
+import os 
+import nltk
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+
+from gensim.models import word2vec
+
+# %%
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en.examples import sentences
+from nltk.corpus import wordnet
+import spacy
+import urllib
+import json
+nlp = spacy.blank('en')
+
+# %%
+class BestSyn:
+
+    def get_datamuse_syn_list(self):
+        url = "https://api.datamuse.com/words?ml=" + self.word
+        response = urllib.request.urlopen(url)
+        data = response.read().decode("utf-8")
+        json_data = json.loads(data)
+        word_list = []
+        for x in json_data:
+            word_list.append(x['word'])
+        return word_list
+
+    def __init__(self, word):
+        self.word = word
+        self.best_score = 0.4
+        self.best_choice = ""
+
+
+    def pull(self):
+        words_list = self.get_datamuse_syn_list()
+        for syn_word in words_list:
+            use_nltk = True
+            try:
+                nltk_raw_word = wordnet.synsets(self.word)[0]
+                nltk_syn_word = wordnet.synsets(syn_word)[0]
+            except:
+                use_nltk = False
+            
+            spacy_raw_word = nlp(self.word.lower())
+            
+            spacy_syn_word = nlp(syn_word.lower())
+
+            
+            spacy_score = spacy_raw_word.similarity(spacy_syn_word)
+            
+            if (use_nltk == True):
+                nltk_score = nltk_syn_word.wup_similarity(nltk_raw_word)
+                if (nltk_score == None):
+                    nltk_score = 0
+                score = (nltk_score+spacy_score)/2
+            else:
+                score = spacy_score
+
+                
+            if (((nltk_score>0.3) & (nltk_score<0.4)) | ((score>0.3) & (self.best_choice==""))):
+                self.best_score = score
+                self.best_choice = syn_word
+        return [self.best_score, self.best_choice]
+
+        
+
+    
+def __del__(self):
+        self.word = False
+        self.best_score = False
+        self.best_choice = False
+
+# %%
+BestSyn('pathetic').pull()[1]
