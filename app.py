d6e65fc1f2258bc5a37242d605c93f4a64a8cd2b
Vaishali Papneja <51495198+VaishaliPapneja@users.noreply.github.com>
2020-07-29 16:50:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 # %%
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns',None)
